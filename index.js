20bf11c960e654b0327769b2fcee0bbfd91e6be7
Sir Eric <echeungsk@gmail.com>
2020-03-30 04:02:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function findMatching(drivers, name){
 function fuzzyMatch(drivers, letters) {
   let reg = new RegExp(`${letters}\\w+`, `ig`);
   return drivers.filter(function (driver) {
-    return driver !== driver.match(reg).toString()});
+    return driver === driver.match(reg).toString()});
 }
 
 function matchName (list, name) {
